eb75eb1bd5b337c2e101c069b8a9c5a22eb54f51
MotuGolu <anjali140698@gmail.com>
2024-07-27 17:37:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+//In fragment to short the code and does not need to call onCreateView we can call layout as constructor in Fragment()
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragments
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn1 :Button = findViewById(R.id.btnFragment1);
+        val btn2 :Button = findViewById(R.id.btnFragment2);
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,firstFragment)
+            commit()
+        }
+
+        btn1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,firstFragment)
+                commit()
+            }
+        }
+        btn2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,secondFragment)
+                commit()
+            }
+        }
+
+
+    }
+}
